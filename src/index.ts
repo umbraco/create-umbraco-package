cf5b88f360e4a324f75885ffd5991356d69688f6
Warren Buckley <warren@umbraco.com>
2023-05-30 10:44:18 +0100

    Add in some test prompts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
+import { input, confirm } from '@inquirer/prompts';
 import chalk from 'chalk';
-
-const log = console.log;
+import figlet from 'figlet';
 
 function displayWelcome(){
-    log(chalk.bgRed.bold('Umbraco Create Package CLI'));
+    console.log(chalk.green.bold(figlet.textSync('Umbraco', { font: 'Thin'})));
 }
 
+async function startPrompts(){
+
+    const answers = {
+        firstName: await input(
+            { 
+                message: "What's your first name?"
+            }),
+        allowEmail: await confirm(
+            { 
+                message: "Can we email you?"
+            })
+    };
+      
+    console.log(answers.firstName);
+    console.log(answers.allowEmail);
+};
+
+
 displayWelcome();
+startPrompts();
