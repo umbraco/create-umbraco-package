6724d74078066cbbfe353d4f960c2f9bfa14c96d
Warren Buckley <warren@umbraco.com>
2023-05-31 11:27:45 +0100

    Ensure do cleanup before copying files back again

--- scripts/copy-scaffold-files.js
+++ scripts/copy-scaffold-files.js
@@ -1,4 +1,9 @@
 import fs from 'fs';
 
+if(fs.existsSync('./dist/scaffolds')) {
+    console.log('Deleting existing scaffold files in ./dist/scaffolds');
+    fs.rmSync('./dist/scaffolds', { recursive: true, force: true });
+}
+
 console.log('Copying scaffold files from ./src/scaffolds to ./dist/scaffolds');
 fs.cpSync('./src/scaffolds', './dist/scaffolds', { recursive: true,  });
